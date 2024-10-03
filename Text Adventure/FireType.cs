083e5f931717bb28f7f94d54a704b895edadc760
MUKILTEO\1722815 <1722815@mukilteo.wednet.edu>
2024-10-03 13:17:54 -0700

    Making Pokemon (almost all the diffrent casses are added) prioritizing the fire type class.

---
+++ Text Adventure/FireType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Adventure
+{
+    internal class FireType
+    {
+        //fire type pokemon
+        
+
+
+
+
+
+     if isinstance(enemy == GrassType)
+        {
+            Console.WriteLine("Attack is Super effective");
+            return self.attacks[attack_name]["power"] * 1.5;
+        }
+    }
+}
---
+++ Text Adventure/MainProgram.cs
@@ -0,0 +1,28 @@
+﻿namespace Text_Adventure
+{
+    internal class MainProgram
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pokemon Battle");
+            string[] deck =
+            {
+            FireType("charmander",60),
+            FireType("Rapidash", 90),
+            FireType("flaryon", 30),
+            WaterType("Squirtle", 100),
+            WaterType("Garydos", 100),
+            WaterType("Tentacool", 100),
+            GrassType("Bulbasaur", 60),
+            GrassType("Oddish", 40),
+            GrassType("Bellsprout", 40)
+            };
+            string User = PlayerCharacter("player");
+            string opponet = Computer("computer");
+
+            for 
+
+        }
+    }
+}
